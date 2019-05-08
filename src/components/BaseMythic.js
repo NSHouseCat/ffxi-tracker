c4c193a8a47d24b48962c1ebe4f88330834032c3
Andrew Nordahl <drewdahl@gmail.com>
2019-05-08 12:45:28 -0700

    Added rest of BaseMythic constants

--- src/components/BaseMythic.js
+++ src/components/BaseMythic.js
@@ -4,6 +4,10 @@ class BaseMythic extends Component {
 
 	render() {
 		const requiredAlexandrite = 30000
+		const requiredIchor = 100000
+		const requiredNyzul = 150000
+		const requiredAssaults = 50
+
 		return(
 			<div>
 				<h1>Base Mythic</h1>
