b4b22cd12249fbe48692fa889f41ecccbcaa918a
Andrew Nordahl <drewdahl@gmail.com>
2019-05-09 01:31:47 -0700

    More fleshing out of BaseMythic component

--- src/components/BaseMythic.js
+++ src/components/BaseMythic.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Progress } from 'react-sweet-progress';
+import "react-sweet-progress/lib/style.css";
+
 
 class BaseMythic extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	getProgress = (currentAmount, amountRequired) => {
+		return ( ( currentAmount / amountRequired ) * 100 )
+	}
+
 
 	render() {
 		const requiredAlexandrite = 30000
@@ -12,9 +25,15 @@ class BaseMythic extends Component {
 			<div>
 				<h1>Base Mythic</h1>
 				<p>{ requiredAlexandrite }</p>
+				<Progress percent={this.getProgress(this.props.alexandrite, requiredAlexandrite)} status="success" />
+				{this.props.alexandrite}
 			</div>
 		)
 	}
+}
+
+BaseMythic.propTypes = {
+	alexandrite: PropTypes.number
 }
 
 export default BaseMythic;
